b709f1786910bd9d8279bc87d4335c01a7706a46
Evgeny V. Zalivochkin <master.gekus@gmail.com>
2017-08-27 21:33:50 +0300

    Addede complex read-only test

--- tests/mgjson_gtest.cpp
+++ tests/mgjson_gtest.cpp
@@ -119,3 +119,21 @@ TEST_F(SharedDataTest, Assignment)
     EXPECT_EQ(b.is_copied(), false);
     EXPECT_EQ(a.is_same(b), true);
 }
+
+TEST_F(SharedDataTest, AssignmentAndCopyMixed)
+{
+    TestSharedData a;
+    EXPECT_EQ(a.value(), 0);
+    TestSharedData b;
+    EXPECT_EQ(b.value(), 0);
+    b = a;
+    EXPECT_EQ(b.value(), 0);
+    TestSharedData c(a);
+    EXPECT_EQ(c.value(), 0);
+
+    EXPECT_EQ(a.is_copied(), false);
+    EXPECT_EQ(b.is_copied(), false);
+    EXPECT_EQ(c.is_copied(), false);
+    EXPECT_EQ(a.is_same(b), true);
+    EXPECT_EQ(a.is_same(c), true);
+}
